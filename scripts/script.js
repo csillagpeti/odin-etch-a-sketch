4a70f8cf3d26d689f7e5cc1aa9701d8668a57b41
csillagpeti <csillagpeti@gmail.com>
2022-09-03 21:14:46 +0200

    cellSize based on cellNumber

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,19 @@ container.className += ('container');
 body.appendChild(container);
 let gridDivs = [];
 
+// drawing the grid
 initGrid();
 function initGrid(cellNumber = 16) {
+    let properSize = 960/cellNumber;
     for (let i = 0; i < cellNumber; i++) {
         gridDivs[i] = document.createElement('div');
+        gridDivs[i].style.width = `${properSize}px`;
+        gridDivs[i].style.height = `${properSize}px`;
         gridDivs[i].classList += ('columnDiv');
         for (let j = 0; j < cellNumber; j++) {
             gridDivs[i][j] = document.createElement('div');
+            gridDivs[i][j].style.width = `${properSize}px`;
+            gridDivs[i][j].style.height = `${properSize}px`;
             gridDivs[i][j].classList += ('gridDiv');
             gridDivs[i][j].addEventListener("mousemove", function (event) {
               //  if (event.buttons > 0) {
@@ -24,6 +30,7 @@ function initGrid(cellNumber = 16) {
 
 }
 
+//start button - resets grid, asks for grid size
 const button = document.getElementById('start');
 button.addEventListener("click", () => {
     const allGridDivs = document.querySelectorAll('.gridDiv');
@@ -37,3 +44,16 @@ button.addEventListener("click", () => {
     cellNumber = prompt('How many cells?');
     initGrid(cellNumber);
 })
+
+//cellSize calculator and setter for fix grid size
+
+/*function resizeCells(cellNumber){
+
+    let columnDivSelector = document.querySelector('.columnDiv');
+    let gridDivSelector = document.querySelector('.gridDiv');
+
+    columnDivSelector.style.width = `${properSize}px`;
+    columnDivSelector.style.height = `${properSize}px`;
+    gridDivSelector.style.width = `${properSize}px`;
+    gridDivSelector.style.height = `${properSize}px`;
+}*/
